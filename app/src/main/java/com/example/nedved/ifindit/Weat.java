5b0e345d1ffb7fbae4dbf87645d95c77e48ea103
pavloZel9 <34425138+pavloZel9@users.noreply.github.com>
2018-02-05 15:11:18 +0200

    Initial commit

---
+++ app/src/main/java/com/example/nedved/ifindit/Weat.java
@@ -0,0 +1,19 @@
+package com.example.nedved.ifindit;
+
+/**
+ * Created by nedved on 23.01.2018.
+ */
+
+public class Weat {
+    public int icon;
+    public String title;
+    public Weat(){
+        super();
+    }
+
+    public Weat(int icon, String title) {
+        super();
+        this.icon = icon;
+        this.title = title;
+    }
+}
